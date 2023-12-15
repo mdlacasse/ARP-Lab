828b0ac6a3006ee62c01bd572ddcb19b24a19d2c
Martin-D. Lacasse <martin.d.lacasse@jhu.edu>
2023-12-15 11:46:09 -0500

    Getting closer to the end. Is this v. 1.0?

---
+++ jack+jill.py
@@ -0,0 +1,158 @@
+'''
+This file is part of the HARP Python program for exploring
+financial retirement decisions.
+
+In this file, we use the same example of Jack and Jill but using
+a command line python script to explore Monte-Carlo simulations
+and historical rates.
+
+Copyright -- Martin-D. Lacasse (2023)
+
+This program comes with no garantee. Use at your own risks.
+
+'''
+import harp as rp
+
+def runOnce(stype, frm, to, plot=False):
+    '''
+    Run one instance of a simulation.
+    '''
+    rp.setVerbose(False)
+
+    plan = rp.Plan(YOB=[1961, 1964], expectancy=[89, 92])
+
+    plan.setAssetBalances(taxable=[60000, 20000],
+                          taxDeferred=[500000, 120000],
+                          taxFree=[80000, 25000],
+                          beneficiary=[1., 1.])
+
+    plan.setInitialAR(taxableAR=[[0, 25, 50, 25], [0, 25, 50, 25]],
+                      taxDeferredAR=[[60, 40, 0, 0], [60, 40, 0, 0]],
+                      taxFreeAR=[[100, 0, 0, 0], [100, 0, 0, 0]])
+
+    plan.setFinalAR(taxableAR=[[0, 50, 50, 0], [0, 50, 50, 0]],
+                    taxDeferredAR=[[60, 40, 0, 0], [60, 40, 0, 0]],
+                    taxFreeAR=[[60, 40, 0, 0], [60, 40, 0, 0]])
+
+    plan.interpolateAR('linear')
+
+    # Specify rate of returns for each class of assets.
+    # Valid entries are 'historical', 'stochastic', 'default',
+    # or a list of fixed average values.
+    # myrates = [6.0, 3.0, 3.0, 3.08]
+    # plan.setRates(myrates)
+    # plan.setRates('default')
+    if stype == 'historical':
+        plan.setRates('historical', frm, to)
+    else:
+        plan.setRates('stochastic', frm, to)
+
+    # Enter desired value for net income in retirementi, and spending profile.
+    plan.setDesiredIncome(79500, 'smile')
+
+    # How to handle withdrawals between spouses.
+    plan.setSpousalSplit('auto')
+
+    # Now enter fixed income.\n",
+    plan.setPension([0, 10000], [65, 65])
+
+    # And social security income.
+    plan.setSocialSecurity([30000, 28000], [70, 70])
+
+    # Name of spreadsheet containing financial events (contributions, etc).
+    plan.readContributions('jack+jill.xlsx')
+
+    plan.run()
+
+    if plot:
+        plan.plotAccounts()
+        # plan.plotTaxableIncome()
+        # plan.plotNetIncome()
+        # plan.plotSources()
+        # plan.plotTaxes()
+        plan.plotRates()
+
+        # plan.showAndSave()
+
+    return plan
+
+##############################################################
+# Performing a historical simulation of N stochastic cases.
+
+def runHistorical(frm):
+    '''
+    Run historical simulation passing through the dreaded
+    1966 year.
+    '''
+    span = 35
+    plot = False
+    to = frm + span
+    N = 2022 - span - frm
+    success = 0
+    total = 0
+    for i in range(N):
+        print('--------------------------------------------')
+        print('Running case #', i, '(', frm+i, ')')
+        plan = runOnce('historical', frm+i, to+i, plot=plot) 
+        print('Plan success:', plan.success)
+        if plan.success:
+            success += 1
+
+        # Assume 30% tax on taxable part of estate.
+        estate, factor = plan.estate(30)
+        print('Estate: (today\'s $)', rp.d(estate),
+              ', cum. infl.:', rp.pc(factor))
+        total += estate
+        if plot:
+            plan.show(5)
+            # plan.showAndSave()
+
+    print('============================================')
+    print('Success rate:', success, 'out of', N, '('+rp.pc(100*success/N)+')')
+    print('Average estate value (today\'s $): ', rp.d(total/N))
+
+
+def runMonteCarlo(N):
+    '''
+    Run n simulations using stochastic sinulation.
+    '''
+    success = 0
+    total = 0
+    for i in range(N):
+        print('--------------------------------------------')
+        print('Running case #', i)
+        plan = runOnce('stochastic', 1940, 2022, plot=False)
+        print('Plan success:', plan.success)
+        if plan.success:
+            success += 1
+
+        # Assume 30% tax on taxable part of estate.
+        estate, factor = plan.estate(30)
+        print('Estate: (today\'s $)', rp.d(estate),
+              ', cum. infl.:', rp.pc(factor))
+        total += estate
+
+    print('============================================')
+    print('Success rate:', success, 'out of', '('+rp.pc(100*success/N)+')')
+    print('Average estate value (today\'s $): ', rp.d(total/N))
+
+
+######################################################################
+
+import sys
+
+# Pick the one you want to run from the command line argument:
+if len(sys.argv) >= 2:
+    if sys.argv[1] == 'historical':
+        runHistorical(1930)
+    elif sys.argv[1] == 'Monte-Carlo':
+        if len(sys.argv) == 3:
+            N = sys.argv[2]
+            runMonteCarlo(N)
+        else:
+            print('Monte-Carlo needs a number.')
+    else:
+        print('Unknown case:', sys.argv[1])
+else:
+    print("Script requires one argument: 'historical' or 'Monte-Carlo'.")
+
